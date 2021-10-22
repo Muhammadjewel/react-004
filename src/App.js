f770075a0c1dfce18ec5e1c6780cace8702697bf
Muhammadjavohir <msuratov@gmail.com>
2021-10-22 09:29:48 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './assets/css/styles.css';
+import Component from "./Components/Component/Component";
+
+function App() {
+  return (
+    <div className="App">
+      <Component />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Component/Component.js
@@ -0,0 +1,13 @@
+import "./Component.css";
+
+function Component () {
+  return (
+    <>
+      <div className="container">
+        <div className="text">Component</div>
+      </div>
+    </>
+  )
+}
+
+export default Component;
